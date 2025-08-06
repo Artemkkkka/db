6e131a4a5c8cfb3439a9c2f49b91fc5a99a302b0
Артём Брагин <bragin15bragin@yandex.ru>
2025-08-06 13:25:02 +0300

    chage config

--- Dev/db/config.py
+++ Dev/db/config.py
@@ -4,8 +4,8 @@ import os
 
 load_dotenv()
 
-DB_NAME = os.environ.get('DB_NAME')
-DB_HOST = os.environ.get('DB_HOST')
-DB_PORT = os.environ.get('DB_PORT')
-DB_USER = os.environ.get('DB_USER')
-DB_PASS = os.environ.get('DB_PASS')
+DB_USER = os.getenv("POSTGRES_USER")
+DB_PASS = os.getenv("POSTGRES_PASSWORD")
+DB_NAME = os.getenv("POSTGRES_DB")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
